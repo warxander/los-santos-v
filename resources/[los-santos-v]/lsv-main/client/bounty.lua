bb9eea7ecbe00cf75dab84ba88bb7a3d525510a7
Alexander Dikanchukov <aleksandr.dikanchukov@iksatprof.ru>
2018-03-22 12:23:05 +0300

    client/bounty: Show "Bounty" scaleform movie

--- resources/[los-santos-v]/lsv-main/client/bounty.lua
+++ resources/[los-santos-v]/lsv-main/client/bounty.lua
@@ -1,5 +1,9 @@
 --TODO Instancing players?
 
+local bountyScaleform = nil
+local bountyScaleformDuration = 5500
+local bountyScaleformTransitionOutDuration = 500
+local bountyText = 'Watch out, someone has put a Bounty on you.'
 
 RegisterNetEvent('lsv:setBounty')
 AddEventHandler('lsv:setBounty', function(bountyServerPlayerId)
@@ -10,10 +14,31 @@ AddEventHandler('lsv:setBounty', function(bountyServerPlayerId)
 	PlaySoundFrontend(-1, "CONFIRM_BEEP", "HUD_MINI_GAME_SOUNDSET", true)
 
 	if GetPlayerServerId(PlayerId()) == bountyServerPlayerId then
-		Gui.DisplayNotification('Watch out, someone has put a Bounty on you.', 'CHAR_LESTER_DEATHWISH', 'Unknown')
-	else
-		Gui.DisplayNotification('A Bounty has been set on '..Gui.GetPlayerName(bountyServerPlayerId, '~p~'))
-	end
+		Gui.DisplayNotification(bountyText, 'CHAR_LESTER_DEATHWISH', 'Unknown')
+
+		bountyScaleform = Scaleform:Request('MIDSIZED_MESSAGE')
+		bountyScaleform:Call('SHOW_SHARD_MIDSIZED_MESSAGE', 'BOUNTY', bountyText)
+
+		local startDrawingTime = GetGameTimer()
+		while GetGameTimer() - startDrawingTime < bountyScaleformDuration do
+			Citizen.Wait(0)
+
+			if GetGameTimer() - startDrawingTime > bountyScaleformDuration - bountyScaleformTransitionOutDuration then
+				bountyScaleform:Call('SHARD_ANIM_OUT', 1, 0.33)
+				startDrawingTime = startDrawingTime + bountyScaleformTransitionOutDuration
+
+				while GetGameTimer() - startDrawingTime < bountyScaleformDuration do
+					Citizen.Wait(0)
+					bountyScaleform:RenderFullscreen(255, 255, 255, 255)
+				end
+
+				bountyScaleform:Delete()
+				return
+			end
+
+			bountyScaleform:RenderFullscreen(255, 255, 255, 255)
+		end
+	else Gui.DisplayNotification('A Bounty has been set on '..Gui.GetPlayerName(bountyServerPlayerId, '~p~')..'.') end
 end)
 
 
