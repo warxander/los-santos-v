c03371df1cea17eeb96b13a26fedfede341e29bf
WarBat <dikanchukov@mail.ru>
2018-04-01 10:43:16 +0300

    client/report: Better reporting reason text

--- resources/[los-santos-v]/lsv-main/client/report.lua
+++ resources/[los-santos-v]/lsv-main/client/report.lua
@@ -1,6 +1,6 @@
 local reportedPlayers = { }
 local reportingPlayer = nil
-local reportingReasons = { "Bad Player Name", "Harassment", "Cheating", "Spam" }
+local reportingReasons = { "Inappropriate Player Name", "Harassment", "Cheating", "Spam" }
 
 
 AddEventHandler('lsv:init', function()
