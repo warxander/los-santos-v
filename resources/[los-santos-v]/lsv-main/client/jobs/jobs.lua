67ec96e69fd7d299162a88500be2487ca3946f57
WarBat <dikanchukov@mail.ru>
2019-01-07 16:00:09 +0300

    client/jobs: Missions are no longer random

--- resources/[los-santos-v]/lsv-main/client/jobs/jobs.lua
+++ resources/[los-santos-v]/lsv-main/client/jobs/jobs.lua
@@ -1,13 +1,21 @@
 local missionPlaces = { }
-local missionNames = { 'MostWanted', 'Headhunter', 'Velocity', 'AssetRecovery', 'MarketManipulation' }
+local missionIds = { 'MostWanted', 'Headhunter', 'Velocity', 'AssetRecovery', 'MarketManipulation' }
+local missionNames = {
+	['MostWanted'] = 'Most Wanted',
+	['Headhunter'] = 'Headhunter',
+	['Velocity'] = 'Velocity',
+	['AssetRecovery'] = 'Asset Recovery',
+	['MarketManipulation'] = 'Market Manipulation',
+}
 
 
 AddEventHandler('lsv:init', function()
 	for _, place in ipairs(Settings.mission.places) do
-		table.insert(missionPlaces, {
-			startTime = nil,
-			blip = Map.CreatePlaceBlip(Blip.Mission(), place.x, place.y, place.z, 'Mission', Color.BlipBlue()),
-		})
+		local mission = { }
+		mission.startTime = nil
+		mission.mission = Utils.GetRandom(missionIds)
+		mission.blip = Map.CreatePlaceBlip(Blip.Mission(), place.x, place.y, place.z, missionNames[mission.mission], Color.BlipBlue())
+		table.insert(missionPlaces, mission)
 	end
 
 	while true do
@@ -20,19 +28,21 @@ AddEventHandler('lsv:init', function()
 				SetBlipAlpha(place.blip, alpha)
 
 				if place.startTime and GetTimeDifference(GetGameTimer(), place.startTime) >= Settings.mission.timeout then
-					Map.SetBlipFlashes(place.blip, 5000)
 					place.startTime = nil
+					place.mission = Utils.GetRandom(missionIds)
+					Map.SetBlipText(place.blip, missionNames[place.mission])
+					Map.SetBlipFlashes(place.blip, 5000)
 				end
 
 				if not JobWatcher.IsAnyJobInProgress() then
 					local playerX, playerY, playerZ = table.unpack(GetEntityCoords(PlayerPedId()))
 					local placeX, placeY, placeZ = table.unpack(GetBlipCoords(place.blip))
 					if not place.startTime and GetDistanceBetweenCoords(playerX, playerY, playerZ, placeX, placeY, placeZ, true) < Settings.placeMarkerRadius then
-						Gui.DisplayHelpText('Press ~INPUT_PICKUP~ to start Mission.')
+						Gui.DisplayHelpText('Press ~INPUT_PICKUP~ to start '..missionNames[place.mission]..'.')
 						if IsControlJustReleased(0, 38) then
 							place.startTime = GetGameTimer()
 							SetBlipAlpha(place.blip, 0)
-							SetTimeout(1000, function() TriggerEvent('lsv:start'..Utils.GetRandom(missionNames)) end)
+							SetTimeout(1000, function() TriggerEvent('lsv:start'..place.mission) end)
 						end
 					end
 				end
