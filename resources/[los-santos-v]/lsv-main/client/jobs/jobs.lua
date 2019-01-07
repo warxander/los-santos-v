6300bab7b5a96b9379aa77a5e259035b7cbc271a
WarBat <dikanchukov@mail.ru>
2019-01-07 22:58:08 +0300

    client/jobs: Update mission blip color

--- resources/[los-santos-v]/lsv-main/client/jobs/jobs.lua
+++ resources/[los-santos-v]/lsv-main/client/jobs/jobs.lua
@@ -14,7 +14,7 @@ AddEventHandler('lsv:init', function()
 		local mission = { }
 		mission.startTime = nil
 		mission.mission = Utils.GetRandom(missionIds)
-		mission.blip = Map.CreatePlaceBlip(Blip.Mission(), place.x, place.y, place.z, missionNames[mission.mission], Color.BlipBlue())
+		mission.blip = Map.CreatePlaceBlip(Blip.Mission(), place.x, place.y, place.z, missionNames[mission.mission], Color.BlipPurple())
 		table.insert(missionPlaces, mission)
 	end
 
